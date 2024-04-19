d05ae48ca9116f8e9281b473e6e971073795a659
Pilosio <103057112+Pilosio@users.noreply.github.com>
2024-04-19 23:41:24 +0200

    Add files via upload

---
+++ PaginaIniziale.php
@@ -0,0 +1,189 @@
+<?php
+
+$host = "localhost";
+$username = "root"; // Sostituisci con il tuo nome utente del database
+$password = ""; // Sostituisci con la tua password del database
+$dbname = "fittofit"; // Sostituisci con il nome del tuo database
+$email = "";
+$nome = "";
+$cognome = "";
+$passwordGLS = "";
+$id = "";
+$flag = true;
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Verifica della connessione
+if ($conn->connect_error) {
+  die("Connessione fallita: " . $conn->connect_error);
+}
+// acquisizione dati dal form HTML
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+  $email = $_POST["email"];
+  //$passwordGLS = md5($passwordGLS);
+  $passwordGLS = $_POST["password"];
+  $passwordGLS = md5($passwordGLS);
+  // lettura della tabella utenti
+
+  if (isset($_POST['Invia'])) {
+    $sql2 = "SELECT * FROM unasession WHERE email='$email'and password='$passwordGLS'";
+    $result2 = $conn->query($sql2);
+
+    $cognome = $result2->fetch_assoc()["cognome"];
+    $result2 = $conn->query($sql2);
+    $nome = $result2->fetch_assoc()["nome"];
+    $result2 = $conn->query($sql2);
+    $id = $result2->fetch_assoc()["id"];
+
+    $sql =  "SELECT * FROM unasession WHERE email='$email'AND password='$passwordGLS'";
+    #Inoltre la password viene crittografata con la funzione md5, applicando l’algoritmo MD5; in questo
+    #modo nella tabella del database la password viene registrata non in chiaro, ma come una stringa di 32
+    #cifre esadecimali (128 bit).
+    if ($conn->query($sql) === TRUE) {
+      //  echo "Dati inseriti con successo nel database!";
+    } else {
+      //  echo "Errore durante l'inserimento dei dati: " . $conn->error;
+      $flag = false;
+    }
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      session_start();
+      $_SESSION['id'] = $id;
+      $_SESSION['email'] = $email;
+      $_SESSION['password'] = $passwordGLS;
+      $_SESSION['nome'] = $nome;
+      $_SESSION['cognome'] = $cognome;
+
+      header("Location: PagClient.php");
+    } else {
+      // echo "Identificazione non riuscita: nome utente o password errati <br />";
+      // echo "Torna a pagina di <a href=\"login.html\">login</a>";
+    }
+  }
+}
+
+
+#provoca il reindirizzamento a un’altra pagina loginok.php, che potrebbe essere la prima pagina del-
+#l’area riservata alla quale possono accedere gli utenti autenticati.
+?>
+
+<html>
+
+<body>
+
+  <title> Login</title>
+
+
+  <h1> FITTO FIT WORLD </h1>
+  
+
+
+  <div>
+    <form action="index.php" method="post">
+
+      <input type="submit" name="Invia" value="LOGIN">
+      <p>  Oppure?</p>
+      <input type="submit" name="Registrazione" value="REGISTRATI">
+      <?php if (isset($_POST['Registrazione'])) {
+        header("Location: registrazione.php");
+      } ?>
+ 
+
+
+    </form>
+
+
+  </div>
+
+</body>
+
+
+
+
+<style>
+  h1 {
+    text-align: center;
+    font-size: 70px;
+    font-weight: 600;
+    font-family: 'Roboto', sans-serif;
+    color: #ffffff;
+    text-transform: uppercase;
+    text-shadow: 1px 1px 0px #957dad,
+      1px 2px 0px #957dad,
+      1px 3px 0px #957dad,
+      1px 4px 0px #957dad,
+      1px 5px 0px #957dad,
+      1px 6px 0px #957dad,
+      1px 10px 5px rgba(16, 16, 16, 0.5),
+      1px 15px 10px rgba(16, 16, 16, 0.4),
+      1px 20px 30px rgba(16, 16, 16, 0.3),
+      1px 25px 50px rgba(16, 16, 16, 0.2);
+  }
+
+  p {
+    text-align: center;
+    font-size: 35px;
+    font-weight: 600;
+    font-family: 'Roboto', sans-serif;
+    color: black;
+    text-transform: uppercase;
+    text-shadow: 1px 1px 0px #957dad,
+      1px 2px 0px #957dad,
+      1px 3px 0px #957dad,
+      1px 4px 0px #957dad,
+      1px 5px 0px #957dad,
+      1px 6px 0px #957dad,
+      1px 10px 5px rgba(16, 16, 16, 0.5),
+      1px 15px 10px rgba(16, 16, 16, 0.4),
+      1px 20px 30px rgba(16, 16, 16, 0.3),
+      1px 25px 50px rgba(16, 16, 16, 0.2);
+  }
+
+
+
+  input,
+  select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+
+  }
+
+  body {
+    background-image: url("Sudore.jpg");
+    background-repeat: no-repeat;
+    background-position: absolute;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+
+
+  input[type=submit] {
+    width: 10%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 840px;
+    border: none;
+    border-radius: 234px;
+    cursor: pointer;
+  }
+
+  input[type=submit]:hover {
+    background-color: #45a049;
+  }
+
+  div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+  }
+</style>
+
+
+</html>
---
+++ fittofit.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 19, 2024 alle 23:40
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fittofit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utente`
+--
+
+CREATE TABLE `utente` (
+  `id` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `cognome` text NOT NULL,
+  `admin` tinyint(4) NOT NULL COMMENT 'Varibiale booleana per il controllo utente',
+  `password` varchar(40) NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `utente`
+--
+ALTER TABLE `utente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
